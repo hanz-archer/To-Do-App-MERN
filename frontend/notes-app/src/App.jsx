7c71b559e9c159dcf37b9224b93868273a633600
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-12-06 10:10:51 +0800

    Routes Added

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path = "/dashboard" exact element={<Home />} />
+      <Route path = "/signup" exact element={<Signup />} />
+      <Route path = "/login" exact element={<Login />} />
+    </Routes>
+  </Router>
+);
+
 
 const App = () => {
-  return (
-    <div><Home /></div>
-  )
-}
+  return <div>{routes}</div>;
+};
 
 export default App
